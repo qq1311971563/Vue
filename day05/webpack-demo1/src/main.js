09b850f7344f273496130b1d1edb3af293c8d731
马栋梁 <1311971563@qq.com>
2018-12-15 12:25:45 +0800

    暂时提交

--- day05/webpack-demo1/src/main.js
+++ day05/webpack-demo1/src/main.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 
 $(function(){
-    $('li:odd').css('backgroundColor','lightblue')
+    $('li:odd').css('backgroundColor','red')
     $('li:even').css('backgroundColor',function(){
         return '#' + 'D97634'
     })
